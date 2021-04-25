618b6624a1a37dd2e0aca32cd512e9ae19f3cf73
Domalega Vladimir <60595331+Domalega@users.noreply.github.com>
2021-04-25 16:34:53 +0700

    Update gauss2.cpp

--- 2 Couse/computing math/gauss2.cpp
+++ 2 Couse/computing math/gauss2.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 
-void printMatrix(float (&m)[3][4]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++)
+const int n = 4;
+
+void printMatrix(float (&m)[n - 1][n]) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n; j++)
             cout << setw(5) << m[i][j];
         cout << endl;
     }
@@ -14,39 +16,37 @@ void printMatrix(float (&m)[3][4]) {
 }
 
 int main() {
-    float matrix[3][4] = { {2, 1, -3, -5}, {-3, 4, 1, 6}, {-1, 2, -3, -4} };
+  float matrix[n - 1][n] = { {2, 1, -3, -5}, {-3, 4, 1, 6}, {-1, 2, -3, -4} };
 	printMatrix(matrix);
 
-    cout.precision(2);
+  cout.precision(2);
 
-    for (int c = 0 ; c < 2 ; c++) { 
-        int max = c ;
-        for (int i = c + 1 ; i < 3 ; i++)  
-            if (abs(matrix[max][c]) < abs(matrix[i][c]))
-                max = i;
-        if (max != c) {
-			swap(matrix[max],  matrix[c]);
-            printMatrix(matrix); 
-        }
-
-        for (int i = c + 1; i < 3; i++) {
-            float coefficient = (matrix[i][c] / matrix[c][c]) * -1;
-            for (int j = c; j < 4; j++) 
-                matrix[i][j] += matrix[c][j] * coefficient;
-        }
-        printMatrix(matrix) ;
+  for (int c = 0 ; c < n - 2 ; c++) { 
+    int max = c ;
+    for (int i = c + 1 ; i < n - 1 ; i++)  
+      if (abs(matrix[max][c]) < abs(matrix[i][c]))
+        max = i;
+      if (max != c) {
+			  swap(matrix[max],  matrix[c]);
+        cout << "Поменяли " << c + 1 << " и " << max + 1 << " строку\n";
+        printMatrix(matrix); 
+      }
+        
+    for (int i = c + 1; i < n - 1; i++) {
+      float coefficient = (matrix[i][c] / matrix[c][c]) * -1;
+      for (int j = c; j < n; j++) 
+        matrix[i][j] += matrix[c][j] * coefficient;
     }
+    cout << "Занулен столбец " << c + 1 << "\n";
+    printMatrix(matrix) ;
+  }
 
-
-    for (int c = 2 ; c > -1; c--) {
-        for (int i = 2 ; i > c; i--)
-            matrix[c][3] -=  matrix[c][i];
-
-        matrix[c][3]	/= matrix[c][c];
-
-        for (int i = 0 ; i < c; i++)
-            matrix[i][c] *= matrix[c][3];
-    }
-    
-    cout << endl;
+  for (int c = n - 2 ; c > -1; c--) {
+    for (int i = n - 2 ; i > c; i--)
+      matrix[c][n - 1] -=  matrix[c][i];
+      matrix[c][n - 1]	/= matrix[c][c];
+      for (int i = 0 ; i < c; i++)
+        matrix[i][c] *= matrix[c][n - 1];
+  }
+  cout << endl;
 }
