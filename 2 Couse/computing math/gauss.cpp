cedb7059621769e63e10a26d20c99f310e974837
Domalega Vladimir <60595331+Domalega@users.noreply.github.com>
2021-04-25 16:25:22 +0700

    Update gauss.cpp

--- 2 Couse/computing math/gauss.cpp
+++ 2 Couse/computing math/gauss.cpp
@@ -3,9 +3,11 @@
 
 using namespace std;
 
-void printMatrix(float (&m)[3][4]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++)
+const int n = 4;
+
+void printMatrix(float (&m)[n - 1][n]) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n; j++)
             cout << setw(5) << m[i][j];
         cout << endl;
     }
@@ -13,32 +15,33 @@ void printMatrix(float (&m)[3][4]) {
 }
 
 int main() {
-    float matrix[3][4] = { {2, 1, -3, -5}, {-3, 4, 1, 6}, {-1, 2, -3, -4} };
+    float matrix[n - 1][n] = { {2, 1, -3, -5}, {-3, 4, 1, 6}, {-1, 2, -3, -4} };
     printMatrix(matrix);
 
     cout.precision(2);
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = i + 1; j < 3; j++) {
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = i + 1; j < n - 1; j++) {
             float coefficient = matrix[j][i] / matrix[i][i];
-            for (int k = i; k < 4; k++)
+            for (int k = i; k < n; k++)
                 matrix[j][k] -= matrix[i][k] * coefficient;
             cout << endl;
         }
+  cout << i + 1 << " столбец занулен \n";
 	printMatrix(matrix);
     }
 
-    for (int i = 2; i >= 0; i--) { 
-        for (int j = 2; j > i; j--) {
-            matrix[i][3] -= matrix[i][j] * matrix[j][3];
+    for (int i = n - 2; i >= 0; i--) { 
+        for (int j = n - 2; j > i; j--) {
+            matrix[i][n - 1] -= matrix[i][j] * matrix[j][n - 1];
          	cout << endl;   
         }
-        matrix[i][3] /= matrix[i][i];
+        matrix[i][n - 1] /= matrix[i][i];
 		printMatrix(matrix);
     }
     
-    for (int i = 0; i < 3; i++) {
-        cout << "X [" << i << "] = " << matrix[i][3] << " ";
+    for (int i = 0; i < n - 1; i++) {
+        cout << "X [" << i << "] = " << matrix[i][n - 1] << " ";
         cout << endl;
     }
     cout << endl;
